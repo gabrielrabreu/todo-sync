28705557dc5ac16abc6ec14fb9e44f174c743797
gabrielrabreu <gabrielrabreu.dev@gmail.com>
2026-07-06 13:47:34 -0300

    chore: setup tanstack devtools

--- frontend/src/app/routes/__root.tsx
+++ frontend/src/app/routes/__root.tsx
@@ -1,9 +1,12 @@
+import { TanStackDevtools } from '@tanstack/react-devtools';
 import type { QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtoolsPanel } from '@tanstack/react-query-devtools';
 import {
   createRootRouteWithContext,
   HeadContent,
   Outlet,
 } from '@tanstack/react-router';
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
 
 type RouteContext = {
   queryClient: QueryClient;
@@ -14,6 +17,18 @@ export const Route = createRootRouteWithContext<RouteContext>()({
     <>
       <HeadContent />
       <Outlet />
+      <TanStackDevtools
+        plugins={[
+          {
+            name: 'TanStack Query',
+            render: <ReactQueryDevtoolsPanel />,
+          },
+          {
+            name: 'TanStack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+        ]}
+      />
     </>
   ),
 });
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,12 +2,14 @@ import path from 'path';
 
 import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
+import { devtools } from '@tanstack/devtools-vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
+    devtools(),
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
