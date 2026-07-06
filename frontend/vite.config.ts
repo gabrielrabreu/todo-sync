c770ad2232340cc444dea9df136b825066fd7bd8
gabrielrabreu <gabrielrabreu.dev@gmail.com>
2026-07-06 11:31:06 -0300

    chore: setup relative paths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import babel from "@rolldown/plugin-babel";
+import path from "path";
 
-// https://vite.dev/config/
+import babel from "@rolldown/plugin-babel";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
